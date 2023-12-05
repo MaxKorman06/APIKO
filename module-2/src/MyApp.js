72cb98835ad74e033e6e28f6d8d8e0ed9b698e2e
MaxKorman06 <mkorman359@gmail.com>
2023-12-05 16:03:13 +0200

    hotfix

--- module-2/src/MyApp.js
+++ module-2/src/MyApp.js
@@ -1,10 +1,10 @@
 import React from "react";
 import AppWrapper from "./Test59/Task1/AppWrapper";
 import AppItem from "./Test59/Task2/AppItem";
-import MyImagine from "./Test59/Task2/MyImagine"
-import Image from "./Test59/Task3/Image"
-import { turtles } from './Test59/Task4/Constants';
-import { getItemDescription } from './Test59/Task4/Utils';
+import MyImagine from "./Test59/Task2/MyImagine";
+import Image from "./Test59/Task3/Image";
+import { turtles } from "./Test59/Task4/Constants";
+import { getItemDescription } from "./Test59/Task4/Utils";
 
 const YourApp = () => {
   return (
@@ -13,23 +13,23 @@ const YourApp = () => {
         <div>Завдання 1</div>
       </AppWrapper>
       <AppItem
-        name="Назва вашого елемента"
-        description="Опис вашого елемента тут..."
+        name="Завдання 2"
+        description="Стандартне лого React"
         Image={MyImagine}
       />
       <Image imgUrl="./logo192.png" />
       <AppWrapper title="React Turtles">
         <div>Завдання 4</div>
-      {turtles.map((turtle, index) => (
-        <AppItem
-          key={index}
-          name={turtle.name}
-          description={getItemDescription(turtle.nickName, turtle.weapon)}
-          Image={MyImagine}
-          imgUrl={turtle.imgUrl}
-        />
-      ))}
-    </AppWrapper>
+        {turtles.map((turtle, index) => (
+          <AppItem
+            key={index}
+            name={turtle.name}
+            description={getItemDescription(turtle.nickName, turtle.weapon)}
+            Image={MyImagine}
+            imgUrl={turtle.imgUrl}
+          />
+        ))}
+      </AppWrapper>
     </div>
   );
 };
